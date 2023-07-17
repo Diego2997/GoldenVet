4cbe580a7381d9bdfa27373dbcb1a095a17bf518
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-16 23:41:51 -0300

    configuracion inicial en el index.js y conexion a base de datos

--- src/database/dbConnetion.js
+++ src/database/dbConnetion.js
@@ -1,1 +1,6 @@
-console.log("hola")
+import {connect} from 'mongoose'
+import { MONGODB_URI } from '../config'
+
+connect(MONGODB_URI,{family: 4})
+.then(resp => console.log(`DB conectada en ${resp.connection.name}`))
+.catch(error => console.log(error))
