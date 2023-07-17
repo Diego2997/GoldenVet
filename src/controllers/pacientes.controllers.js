222c6de96a47ded8aae23ac5e9452dd28fbaa808
Silvio Ruiz <silviojrck@gmail.com>
2023-07-17 02:07:40 -0300

    Agrega metodos para crear y obtener un paciente

--- src/controllers/pacientes.controllers.js
+++ src/controllers/pacientes.controllers.js
@@ -11,3 +11,30 @@ export const obtenerPacientes = async (req, res) => {
         });
     }
 }
+
+export const obtenerPaciente = async (req, res) => {
+    try {
+        const paciente = await Paciente.findById(req.params.id);
+        res.status(200).json(paciente);
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            mensaje: "Error al obtener el paciente"
+        });
+    }
+}
+
+export const crearPaciente = async (req, res) => {
+    try {
+        const pacienteNuevo = new Paciente(req.body);
+        await pacienteNuevo.save();
+        res.status(201).json({
+            mensaje: "El paciente se creó correctamente"
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({
+            mensaje: "Error al crear el paciente"
+        });
+    }
+}
--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { obtenerPacientes } from "../controllers/pacientes.controllers";
+import { crearPaciente, obtenerPaciente, obtenerPacientes } from "../controllers/pacientes.controllers";
 
 const router = Router();
 
-router.route('/pacientes').get(obtenerPacientes);
+router.route('/pacientes')
+    .get(obtenerPacientes)
+    .post(crearPaciente);
+    
+router.route('/pacientes/:id').get(obtenerPaciente);
 
 export default router;
