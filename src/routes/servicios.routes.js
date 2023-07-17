22576e80f2116578f117f4823651de7640623210
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-17 02:24:51 -0300

    agregado validaciones para el crud de servicios

--- src/routes/servicios.routes.js
+++ src/routes/servicios.routes.js
@@ -6,14 +6,16 @@ import {
   obtenerServicio,
   obtenerunServicio,
 } from "../controllers/servicios.controllers";
+import validarServicio from "../helpers/validarServicio";
+
 
 const router = Router();
 
-router.route("/servicios").get(obtenerServicio).post(crearServicio);
+router.route("/servicios").get(obtenerServicio).post(validarServicio,crearServicio);
 router
   .route("/servicios/:id")
   .delete(borrarServicio)
-  .put(editarServicio)
+  .put(validarServicio, editarServicio)
   .get(obtenerunServicio);
 
 export default router;
