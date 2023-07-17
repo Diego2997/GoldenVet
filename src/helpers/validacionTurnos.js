7bb6ee7ebf6b34c4862e33cb4f5e006a9e37efc6
carabajal-facundo <facarabajal@hotmail.com>
2023-07-17 19:15:54 -0300

    Se agrego a las validaciones respecto a la fecha y hora y que solo reciba le lenguaje alfabetico y no alfanumerico

--- src/helpers/validacionTurnos.js
+++ src/helpers/validacionTurnos.js
@@ -6,21 +6,29 @@ export const validacionTurno = [
         .notEmpty()
         .withMessage("El detalle de la visita es obligatorio")
         .isLength({min: 5, max: 150})
-        .withMessage("El detalle debe de contener entre 5 y 150 caracteres"),
+        .withMessage("El detalle debe de contener entre 5 y 150 caracteres")
+        .isAlpha('en-US', {ignore: '\s'})
+        .withMessage("Deve de contener solo letras y espacios"),
     check("veterinario")
         .notEmpty()
         .withMessage("El nombre del veterinario es obligatorio")
         .isLength({min: 3, max: 100})
-        .withMessage("La nombre del veterinario debe de contener entre 3 y 50 caracteres"),
+        .withMessage("eL nombre del veterinario debe de contener entre 3 y 50 caracteres")
+        .isAlpha('en-US', {ignore: '\s'})
+        .withMessage("Deve de contener solo letras y espacios"),
     check("paciente")
         .notEmpty()
         .withMessage("El nombre del paciente es obligatorio")
         .isLength({min: 2, max: 50})
-        .withMessage("La nombre del paciente debe de contener entre 2 y 50 caracteres"),
+        .withMessage("La nombre del paciente debe de contener entre 2 y 50 caracteres")
+        .isAlpha('en-US', {ignore: '\s'})
+        .withMessage("Deve de contener solo letras y espacios"),
     check("fechaYHora")
         .notEmpty()
         .withMessage("la fecha y la hora es obligatoria")
         .isLength({min: 14, max: 20})
-        .withMessage("La tarea debe de contener entre 5 y 100 caracteres"),
+        .withMessage("La fecha debe de contener entre 14 y 20 caracteres")
+        .matches(/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})(\s)([0-1][0-9]|2[0-3])(:)([0-5][0-9])$/)
+        .withMessage("La fecha y hora deve de tener el siguiente formato: dd/mm/yyyy hh:mm o dd-mm-yyyy hh:mm"),
     (req,res,next)=>{resultadoValidacion(req,res,next)}
 ];
