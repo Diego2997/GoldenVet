f41a313d6971795fdc0fc3425cf95a6c01ecff3e
Silvio Ruiz Cormack <87553400+silviojrcc@users.noreply.github.com>
2023-07-17 22:59:32 -0300

    Arregla errores de escritura en validacionTurnos.js

--- src/helpers/validacionTurnos.js
+++ src/helpers/validacionTurnos.js
@@ -8,27 +8,27 @@ export const validacionTurno = [
         .isLength({min: 5, max: 150})
         .withMessage("El detalle debe de contener entre 5 y 150 caracteres")
         .isAlpha('en-US', {ignore: '\s'})
-        .withMessage("Deve de contener solo letras y espacios"),
+        .withMessage("El detalle de la visita debe contener solo letras y espacios"),
     check("veterinario")
         .notEmpty()
         .withMessage("El nombre del veterinario es obligatorio")
         .isLength({min: 3, max: 100})
-        .withMessage("eL nombre del veterinario debe de contener entre 3 y 50 caracteres")
+        .withMessage("El nombre del veterinario debe de contener entre 3 y 50 caracteres")
         .isAlpha('en-US', {ignore: '\s'})
-        .withMessage("Deve de contener solo letras y espacios"),
+        .withMessage("El nombre del veterinario debe contener solo letras y espacios"),
     check("paciente")
         .notEmpty()
         .withMessage("El nombre del paciente es obligatorio")
         .isLength({min: 2, max: 50})
-        .withMessage("La nombre del paciente debe de contener entre 2 y 50 caracteres")
+        .withMessage("El nombre del paciente debe de contener entre 2 y 50 caracteres")
         .isAlpha('en-US', {ignore: '\s'})
-        .withMessage("Deve de contener solo letras y espacios"),
+        .withMessage("El nombre del paciente debe contener solo letras y espacios"),
     check("fechaYHora")
         .notEmpty()
-        .withMessage("la fecha y la hora es obligatoria")
+        .withMessage("La fecha y la hora es obligatoria")
         .isLength({min: 14, max: 20})
         .withMessage("La fecha debe de contener entre 14 y 20 caracteres")
         .matches(/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})(\s)([0-1][0-9]|2[0-3])(:)([0-5][0-9])$/)
-        .withMessage("La fecha y hora deve de tener el siguiente formato: dd/mm/yyyy hh:mm o dd-mm-yyyy hh:mm"),
+        .withMessage("La fecha y hora debe de tener el siguiente formato: dd/mm/yyyy hh:mm o dd-mm-yyyy hh:mm"),
     (req,res,next)=>{resultadoValidacion(req,res,next)}
 ];
