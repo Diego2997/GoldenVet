ca1137527758223e05229d8694a1e8e11e2bec94
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-07-18 02:29:13 -0300

    se agrega editar un usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,12 +3,13 @@ import {
   crearUsuario,
   obtenerUsuarios,
   obtenerUsuario,
+  editarUsuario
 } from "../controllers/usuarios.controllers";
 
 const router = Router();
 
 router.route("/usuarios").get(obtenerUsuarios).post(crearUsuario);
 
-router.route("/usuarios/:id").get(obtenerUsuario);
+router.route("/usuarios/:id").get(obtenerUsuario).put(editarUsuario);
 
 export default router;
