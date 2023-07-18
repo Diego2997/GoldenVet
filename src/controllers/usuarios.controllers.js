8d9f8632d3bf3f4a2c612069c0983634a75eb639
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-07-18 01:33:48 -0300

    se crea schema de usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,1 +1,13 @@
-console.log("hola")
+import Usuario from "../models/usuario";
+
+const obtenerUsuario = async (req, res)=>{
+    try{
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    }catch(error){
+        console.log(error)
+        res.status(404).json({
+            mensaje: "Error al obtener usuarios"
+        })
+    }
+}
--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,1 +1,46 @@
-console.log("hola")
+import { Schema, model } from "mongoose";
+
+const usuarioSchema = new Schema({
+  nombreUsuario: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 16,
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,16}$/.test(
+          value
+        );
+      },
+      message:
+        "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un carácter especial y un número. Además, la longitud máxima es de 16 caracteres.",
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 15,
+    maxLength: 50,
+    validate: {
+      validator: function (value) {
+        return /\S+@\S+\.\S+/.test(value);
+      },
+      message: "El email no es válido.",
+    },
+  },
+  rol: {
+    type: String,
+    required: true,
+  },
+});
+
+const Usuario = model("usuario", usuarioSchema);
+export default Usuario;
