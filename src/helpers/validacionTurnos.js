a2ee44987a8e05e77e1fbd1493847ddf47a9bb7a
Silvio Ruiz <silviojrck@gmail.com>
2023-07-20 12:28:28 -0300

    Agrega validacion para que los turnos sean solamente de lunes a viernes

--- src/helpers/validacionTurnos.js
+++ src/helpers/validacionTurnos.js
@@ -28,29 +28,36 @@ export const validacionTurno = [
         .withMessage("La fecha y la hora es obligatoria")
         .matches(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s([0-1][0-9]|2[0-3]):(00|30)$/)
         .withMessage("La fecha y hora deben tener el siguiente formato: dd/mm/yyyy hh:00 o dd-mm-yyyy hh:30")
-    .custom((value) => {
-        const parsedDate = new Date(value);
-        if (isNaN(parsedDate.getTime())) {
-            throw new Error("La fecha y hora deben ser una fecha válida");
-        }
-        return true;
-    })
-    .custom((value) => {
-        const fechaHora = new Date(value);
-        const hora = fechaHora.getHours();
-        const minutos = fechaHora.getMinutes();
-        if ((hora < 8 || hora > 20) || (hora == 20 && minutos == 30)) {
-            throw new Error("El turno debe estar en el rango de 8 a 20 horas");
-        }
-        return true;
-    })
-    .custom(async (value, { req }) => {
-        const fechaCreacion = new Date();
-        const fechaHora = new Date(value);
-        if (fechaHora < fechaCreacion) {
-            throw new Error("No se puede crear un turno anterior a la fecha de creación");
-        }
-        return true;
-    }),
+        .custom((value) => {
+            const parsedDate = new Date(value);
+            if (isNaN(parsedDate.getTime())) {
+                throw new Error("La fecha y hora deben ser una fecha válida");
+            }
+            return true;
+        })
+        .custom((value) => {
+            const fechaHora = new Date(value);
+            const hora = fechaHora.getHours();
+            const minutos = fechaHora.getMinutes();
+            if ((hora < 8 || hora > 20) || (hora == 20 && minutos == 30)) {
+                throw new Error("El turno debe estar en el rango de 8 a 20 horas");
+            }
+            return true;
+        })
+        .custom((value) => {
+            const fechaCreacion = new Date();
+            const fechaHora = new Date(value);
+            if (fechaHora < fechaCreacion) {
+                throw new Error("No es posible programar un turno para una fecha y hora anterior a la actual");
+            }
+            return true;
+        })
+        .custom((value) => {
+            const fechaHora = new Date(value);
+            if (fechaHora.getDay() == 0 || fechaHora.getDay() == 6) {
+                throw new Error("Los turnos solo pueden entregarse de lunes a viernes");
+            }
+            return true;
+        }),
     (req,res,next)=>{resultadoValidacion(req,res,next)}
 ];
