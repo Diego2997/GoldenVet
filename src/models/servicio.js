23172fba217d7727fcb11b92f5c55d8f52f2dc2c
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-26 17:23:24 -0300

    se modifico el modelo de servicios para contener subservicios dentro

--- src/models/servicio.js
+++ src/models/servicio.js
@@ -1,32 +1,49 @@
-import { Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
 const servicioSchema = new Schema({
-
-    nombreServicio :{
-        type:String,
-        required:true,
-        minLength:2,
-        maxLength:30,
-        unique:true
+  nombreServicio: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+    
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 600,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  subservicios: [{
+    nombreSubservicio: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 100,
     },
-    descripcion :{
-        type:String,
-        required:true,
-        minLength:2,
-        maxLength:600        
+    descripcionSubservicio: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 600,
     },
-    imagen :{
-        type: String,
-        required: true,
-    }
+    imagenSubservicio: {
+      type: String,
+      required: true,
+    },
+  }],
 });
 
 servicioSchema.set('toJSON', {
-	transform: (documento, returnedObject) => {
-		returnedObject.id = returnedObject._id;
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
+  transform: (documento, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 const Servicio = model("servicio", servicioSchema);
