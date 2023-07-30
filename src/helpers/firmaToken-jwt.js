0c915fb0ffd35610f6f655367a90f56e8e74e154
Silvio Ruiz <silviojrck@gmail.com>
2023-07-30 13:27:32 -0300

    Agrega rol a los datos del token. Cambia a 8 horas el tiempo de expiración

--- src/helpers/firmaToken-jwt.js
+++ src/helpers/firmaToken-jwt.js
@@ -1,18 +1,23 @@
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const generarJWT = (email, nombreUsuario)=>{
-    return new Promise( (resolve, reject)=>{
-        const datos = {email, nombreUsuario};
-        jwt.sign(datos,process.env.SECRET_JWT,{
-            expiresIn: '2h'
-        },(error, token)=>{
-            if(error){
-                console.log(error);
-                reject('No se pudo generar el token')
+const generarJWT = (email, nombreUsuario, rol) => {
+    return new Promise((resolve, reject) => {
+        const datos = { email, nombreUsuario, rol };
+        jwt.sign(
+            datos,
+            process.env.SECRET_JWT,
+            {
+                expiresIn: '8h'
+            },
+            (error, token) => {
+                if (error) {
+                    console.log(error);
+                    reject('No se pudo generar el token');
+                }
+                resolve(token);
             }
-            resolve(token);
-        })
-    })
-}
+        );
+    });
+};
 
-export default generarJWT
+export default generarJWT;
