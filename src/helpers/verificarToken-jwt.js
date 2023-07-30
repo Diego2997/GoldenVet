053737c5bf2bd24fd5838c4a9497bcca11266bd5
Silvio Ruiz <silviojrck@gmail.com>
2023-07-30 13:24:55 -0300

    modifica verificar token para que acepte el rol del usuario

--- src/helpers/verificarToken-jwt.js
+++ src/helpers/verificarToken-jwt.js
@@ -1,24 +1,25 @@
 import jwt from 'jsonwebtoken'
 
-const validarJWT = (req, res, next)=>{
+const validarJWT = (req, res, next) => {
     const token = req.header('x-token');
-    if(!token){
+    if (!token) {
         return res.status(401).json({
-            mensaje: 'No hay token en la peticion'
-        })
+            mensaje: 'No hay token en la petición'
+        });
     }
-    try{
-        const datos =  jwt.verify(token, process.env.SECRET_JWT)
+    try {
+        const datos = jwt.verify(token, process.env.SECRET_JWT);
         req.id = datos.uid;
         req.nombreUsuario = datos.nombreUsuario;
-    }catch(error){
-        console.log(error)
+        req.rol = datos.rol;
+    } catch (error) {
+        console.log(error);
         return res.status(401).json({
-            mensaje: 'El token no es valido'
-        })
+            mensaje: 'El token no es válido'
+        });
     }
 
     next();
-}
+};
 
 export default validarJWT;
